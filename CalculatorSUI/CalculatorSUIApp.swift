24a16b39d2808c9c6b00d4ff711354d8d9ab408c
Siarheimasiuk <winst73@gmail.com>
2024-01-03 20:44:09 +0300

    Initial Commit

---
+++ CalculatorSUI/CalculatorSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSUIApp.swift
+//  CalculatorSUI
+//
+//  Created by Sergio on 3.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
